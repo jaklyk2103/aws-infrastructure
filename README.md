fe2d3915398d8a8f24e239b4f48a29123c4335d0
Jakub <kubeckk21@gmail.com>
2023-08-20 13:10:44 +0200

    created template, added script for migrating db table from another project

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # aws-infrastructure
+
+This project is used to setup AWS resources used by other AWS applications.
+
+- DynamoDB Tables:
+  - Prod
+  - Dev
---
+++ scripts/migrate-dynamodb-table.js
@@ -0,0 +1,38 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, ScanCommand, PutCommand } from '@aws-sdk/lib-dynamodb';
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const exportItemsTableName = 'expenses-test'
+const importItemsTableName = 'prod';
+
+const migrateDatabaseHandler = async () => {
+    var params = {
+        TableName : exportItemsTableName
+    };
+
+    try {
+        var data = await ddbDocClient.send(new ScanCommand(params));
+        console.log(`Retrieved all items from database: ${JSON.stringify(data.Items)}`)
+    } catch (err) {
+        console.log("Error", err);
+    }
+
+    for await (const item of data.Items) { 
+      console.log(`item: ${JSON.stringify(item)}`)
+
+      var params = {
+        TableName : importItemsTableName,
+        Item: { ...item }
+      }
+  
+      try {
+        const data = await ddbDocClient.send(new PutCommand(params));
+        console.log("Success - item added or updated", data);
+      } catch (err) {
+        console.log("Error", err);
+      }
+    }
+  };
+
+migrateDatabaseHandler().then(() => console.log('Migrated completed successfully.'));
